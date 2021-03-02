8c79dc20bdcf86095c5ca595dfa75caffd10ff8b
Muhammad Zul Marij Rizqi Fathullah <muhammadzulmarijrizqifathullah@gmail.com>
2021-03-02 11:34:39 +0700

    Update getTopFollowers.py

--- src/getTopFollowers.py
+++ src/getTopFollowers.py
@@ -1,16 +1,14 @@
 """
-   Copyright 2020 Yufan You <https://github.com/ouuan>
-
+   Copyright 2020-2021 Yufan You <https://github.com/ouuan>
    Licensed under the Apache License, Version 2.0 (the "License");
    you may not use this file except in compliance with the License.
    You may obtain a copy of the License at
-
        http://www.apache.org/licenses/LICENSE-2.0
-
    Unless required by applicable law or agreed to in writing, software
    distributed under the License is distributed on an "AS IS" BASIS,
    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-   See the License for the specific language governing permissions and limitations under the License.
+   See the License for the specific language governing permissions and
+   limitations under the License.
 """
 
 import requests
@@ -46,7 +44,7 @@ if __name__ == "__main__":
 
     html = "<table>\n"
 
-    for i in range(min(len(followers), 14)):
+    for i in range(min(len(followers), 21)):
         login = followers[i][1]
         id = followers[i][2]
         name = followers[i][3]
